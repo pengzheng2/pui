3c4dac43a495d9be9ef3b8a913719dfefa662f0b
“pengzheng2” <pzheng2@andrew.cmu.edu>
2018-10-05 22:59:02 -0400

    1005 commit

--- week6/main.js
+++ week6/main.js
@@ -2,3 +2,12 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+	  // append the input value within an li element
+        list.append("<li>" + itemInput.val() + "</li>"); 
+    });
+});
