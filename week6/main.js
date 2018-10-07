721938c5645b32f9a360c0b1ea92b9a3edc8b7e8
“pengzheng2” <pzheng2@andrew.cmu.edu>
2018-10-07 01:30:57 -0400

    Doing week 6.

--- week6/main.js
+++ week6/main.js
@@ -2,18 +2,19 @@ function addNewList() {
     alert('hello world!');
 }
 
-function addListItem() {
-	var list = document.getElementById("grocery-list");
-	var itemInput = document.getElementById("new-list-item");
-	var newItem = document.createElement("li");
-	newItem.appendChild(document.createTextNode(itemInput.value));
-	list.appendChild(newItem);
-}
+// function addListItem() {
+//     console.log('hello world');
+// 	var list = document.getElementById("grocery-list");
+// 	var itemInput = document.getElementById("new-list-item");
+// 	var newItem = document.createElement("li");
+// 	newItem.appendChild(document.createTextNode(itemInput.value));
+// 	list.appendChild(newItem);
+// }
 
-function deleteListItem(item) {
-	    // remove li element (item) from ol element (item.parentNode)
- 	    item.parentNode.removeChild(item);
-}
+// function deleteListItem(item) {
+// 	    // remove li element (item) from ol element (item.parentNode)
+//  	    item.remove();
+// }
 
 function completeListItem(item) {
     if (item.checked) { // true if the input checkbox is checked
@@ -24,4 +25,30 @@ function completeListItem(item) {
         // in css, this would be: "text-decoration: none"
     }
 }
+
+// $(document).ready(function(){
+//     $("#add-item").click(function() {  // bind handler for click event
+//         var list = $("#grocery-list"); // get the ol list by id
+//         var itemInput = $("#new-list-item"); // get the new item input
+//         // append the input value within an li element
+//         list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>")
+//     });
+
+//     $(".delete-item").click(function() {
+//         console.log("test")
+//         $(this).parent().remove();
+//     });
+
+// });
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
 
